433dc64ae5dff48b3296923fc21b6f1f434a05c2
Akhilesh Garg <akgarg0472@gmail.com>
2021-10-04 22:03:24 +0530

    Initial commit

---
+++ src/main/java/com/akgarg/apachecassandraloader/ApacheCassandraLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.akgarg.apachecassandraloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheCassandraLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApacheCassandraLoaderApplication.class, args);
+	}
+
+}
